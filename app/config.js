9584413f0d47b326ae5c7c25488de2dd4e153739
fernando ariel franco <82460657+Atreesin@users.noreply.github.com>
2026-04-28 15:44:21 -0300

    Update database config to include DB_PORT
    Added DB_PORT to database configuration.

--- app/config.js
+++ app/config.js
@@ -15,6 +15,7 @@ export const ENTRENADOR_USER_LEVEL = process.env.ENTRENADOR_USER_LEVEL
 
 export const database = {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
