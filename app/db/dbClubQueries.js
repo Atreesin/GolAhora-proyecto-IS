58711f70a6fc132c6cf01dcaa81e5e3ec37a6fd8
fernando ariel franco <82460657+Atreesin@users.noreply.github.com>
2026-05-27 22:30:12 -0300

    fix update

--- app/db/dbClubQueries.js
+++ app/db/dbClubQueries.js
@@ -2,15 +2,15 @@ import pool from "./databaseConnection.js"
 
 
 //registro
-const insertClub = 'INSERT INTO clubes SET ?';
+const insertClub = `INSERT INTO clubes SET ?`;
 
 //consulta
-const selectFullInfoClubes = 'SELECT c.id_club as id, c.nombre, c.CUIT, c.num_telefonico as telefono, c.email,  JSON_OBJECT("id", d.id_direccion, "calle", d.calle, "numero", d.numero, "cp", d.codigo_postal, "localidad", l.nombre, "ciudad", ci.nombre, "provincia", pr.nombre, "pais", pa.nombre) AS direccion FROM clubes c LEFT JOIN direcciones d ON c.id_direccion = d.id_direccion LEFT JOIN localidades l ON d.id_localidad = l.id_localidad LEFT JOIN ciudades ci ON l.id_ciudad = ci.id_ciudad LEFT JOIN provincias pr ON ci.id_provincia = pr.id_provincia LEFT JOIN paises pa ON pr.id_pais = pa.id_pais';
-const selectClubes = 'SELECT c.id_club as id, c.nombre, c.CUIT FROM clubes c';
-const selectClubById = 'SELECT c.id_club as id, c.nombre, c.CUIT FROM clubes c WHERE c.id_club = ?';
-const selectFullInfoClubById = 'SELECT c.id_club as id, c.nombre, c.CUIT, c.num_telefonico as telefono, c.email,  JSON_OBJECT("id", d.id_direccion, "calle", d.calle, "numero", d.numero, "cp", d.codigo_postal, "localidad", l.nombre, "ciudad", ci.nombre, "provincia", pr.nombre, "pais", pa.nombre) AS direccion FROM clubes c LEFT JOIN direcciones d ON c.id_direccion = d.id_direccion LEFT JOIN localidades l ON d.id_localidad = l.id_localidad LEFT JOIN ciudades ci ON l.id_ciudad = ci.id_ciudad LEFT JOIN provincias pr ON ci.id_provincia = pr.id_provincia LEFT JOIN paises pa ON pr.id_pais = pa.id_pais WHERE c.id_club = ?';
+const selectFullInfoClubes = `SELECT c.id_club as id, c.nombre, c.CUIT, c.num_telefonico as telefono, c.email,  JSON_OBJECT('id', d.id_direccion, 'calle', d.calle, 'numero', d.numero, 'cp', d.codigo_postal, 'localidad', l.nombre, 'ciudad', ci.nombre, 'provincia', pr.nombre, 'pais', pa.nombre) AS direccion FROM clubes c LEFT JOIN direcciones d ON c.id_direccion = d.id_direccion LEFT JOIN localidades l ON d.id_localidad = l.id_localidad LEFT JOIN ciudades ci ON l.id_ciudad = ci.id_ciudad LEFT JOIN provincias pr ON ci.id_provincia = pr.id_provincia LEFT JOIN paises pa ON pr.id_pais = pa.id_pais`;
+const selectClubes = `SELECT c.id_club as id, c.nombre, c.CUIT FROM clubes c`;
+const selectClubById = `SELECT c.id_club as id, c.nombre, c.CUIT FROM clubes c WHERE c.id_club = ?`;
+const selectFullInfoClubById = `SELECT c.id_club as id, c.nombre, c.CUIT, c.num_telefonico as telefono, c.email,  JSON_OBJECT('id', d.id_direccion, 'calle', d.calle, 'numero', d.numero, 'cp', d.codigo_postal, 'localidad', l.nombre, 'ciudad', ci.nombre, 'provincia', pr.nombre, 'pais', pa.nombre) AS direccion FROM clubes c LEFT JOIN direcciones d ON c.id_direccion = d.id_direccion LEFT JOIN localidades l ON d.id_localidad = l.id_localidad LEFT JOIN ciudades ci ON l.id_ciudad = ci.id_ciudad LEFT JOIN provincias pr ON ci.id_provincia = pr.id_provincia LEFT JOIN paises pa ON pr.id_pais = pa.id_pais WHERE c.id_club = ?`;
 //modificar
-const updateNombreClubById = 'UPDATE nombre FROM clubes WHERE id_club = ?';
+const updateNombreClubById = `UPDATE nombre FROM clubes WHERE id_club = ?`;
 
 
 
