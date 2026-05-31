7507f5bb93726bc1c6d3289f8d068aafbce6aa11
Piuque Ailin Carteau <piuqueailincarteau@gmail.com>
2026-05-31 20:54:38 -0300

    Update ListarCanchas.js

--- app/public/testPiu/js/ListarCanchas.js
+++ app/public/testPiu/js/ListarCanchas.js
@@ -21,7 +21,7 @@ class Cancha {
             <div class="card text-dark shadow border-0 p-4 w-100" style="max-width: 900px; border-radius: 15px; background-color: #ffffff !important;">
                 <div class="d-flex justify-content-between align-items-center flex-wrap">
                 <div class="col-md-5">
-                        <img src="https://golahora-proyecto-is.onrender.com${this.imagen_url}" 
+                        <img src="https://golahora-proyecto-is.onrender.com/api${this.imagen_url}" 
                              class="card-img w-100" 
                              style="height: 280px; object-fit: cover;" 
                              alt="${this.tipo_cancha}">
