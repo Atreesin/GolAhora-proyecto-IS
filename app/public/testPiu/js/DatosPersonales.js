9cc09639e9c768596f1b511431ba63f5e2272366
Piuque Ailin Carteau <piuqueailincarteau@gmail.com>
2026-05-31 10:45:00 -0300

    Update DatosPersonales.js

--- app/public/testPiu/js/DatosPersonales.js
+++ app/public/testPiu/js/DatosPersonales.js
@@ -31,7 +31,7 @@ async function ObtenerDatosPersonales(){
         CampoFecha.value = Datos.fecha_nacimiento;
         CampoTelefono.value = Datos.telefono;
         CampoEmail.value = Datos.email;
-        CampoDireccion.value = `${Datos.calle} ${Datos.numero}, ${Datos.localidad}, ${Datos.pais}`;
+        CampoDireccion.value = Datos.calle;
     }catch(error){
         console.error("Hubo un error al obtener los datos:", error);
     };
