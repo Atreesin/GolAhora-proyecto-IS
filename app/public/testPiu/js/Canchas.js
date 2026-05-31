3fb1d8d7d9c48890b1a2b8ae85e14739dee04817
Piuque Ailin Carteau <piuqueailincarteau@gmail.com>
2026-05-30 22:36:05 -0300

    Update Canchas.js

--- app/public/testPiu/js/Canchas.js
+++ app/public/testPiu/js/Canchas.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const nombreClub = cancha.club.nombre || "Club No Especificado"; // "Club Atlético River Plate"
             
             // Accedemos de forma segura al objeto interno 'tipo_cancha'
-            const infoTipo = cancha.tipo_cancha ? ; 
+            const infoTipo = cancha.tipo_cancha  ; 
 
             // 5. Dibujamos la tarjeta individual en pantalla
             const fila = document.createElement("div");
