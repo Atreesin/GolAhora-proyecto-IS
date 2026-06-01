1d934d9d21680c7f5190b0ce79a6969e2b0c66fb
Fernando Franco <franco.fernando.a@gmail.com>
2026-06-01 15:47:08 -0300

    Update cliente.routes.js

--- app/routes/cliente.routes.js
+++ app/routes/cliente.routes.js
@@ -8,6 +8,7 @@ const router = Router()
 
 router.get("/Perfil", authorization.soloUsers, (req,res)=>res.sendFile(__dirname + "/pages/user/InterfazCliente.html"))
 router.get("/Canchas", authorization.soloUsers, (req,res)=>res.sendFile(__dirname + "/pages/user/canchas.html"))
+router.get("/Cancha", authorization.soloUsers, (req,res)=>res.sendFile(__dirname + "/pages/user/canchas.html"))
 router.get("/Cobro", authorization.soloUsers, (req,res)=>res.sendFile(__dirname + "/pages/user/Cobro.html"))
 router.get("/MisClases", authorization.soloUsers, (req,res)=>res.sendFile(__dirname + "/pages/user/MisClases.html"))
 router.get("/MisEntrenamientos", authorization.soloUsers, (req,res)=>res.sendFile(__dirname + "/pages/user/MisEntrenamientoshtml"))
